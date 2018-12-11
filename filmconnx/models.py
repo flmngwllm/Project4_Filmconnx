a19c2bdf2a1b201e2633ed4e59ae7b2bcd5797e5
Will <flmngwllm232@gmail.com>
2018-12-10 19:03:21 -0500

    creating models

--- filmconnx/models.py
+++ filmconnx/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    photo_url = models.CharField(max_length=400)
+    location = models.CharField(max_length=255)
+    projects = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=400)
+    bio = models.CharField(max_length=255)
+    job_title = models.CharField(max_length=255)
+    union = models.CharField(max_length=255)
+    imbd = models.CharField(max_length=400)
+    availabilty = models.CharField(max_length=255)
+    resume = models.CharField(max_length=255)
+    department = models.CharField(max_length=400)
+    afiliated_groups = models.CharField(max_length=400)
+
+
+
+class Jobs(models.Model):
+    title = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    preview_url = models.CharField(max_length=400)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+
+
+class Poster(models.Model):
+        name = models.CharField(max_length=255)
+        photo_url = models.CharField(max_length=400)
+        nationality = models.CharField(max_length=255)
--- filmconnx/views.py
+++ filmconnx/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from .serializers import ArtistSerializer, SongSerializer
+from .models import Artist, Song
+
+
+class ArtistView(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+class SongView(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 # Create your views here.
