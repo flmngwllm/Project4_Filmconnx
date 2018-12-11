2c89d6d7e89cdc1ce9f9735dd9a2aac30c506d23
Will <flmngwllm232@gmail.com>
2018-12-10 19:08:29 -0500

    finished models

--- filmconnx/models.py
+++ filmconnx/models.py
@@ -16,15 +16,30 @@ class User(models.Model):
     afiliated_groups = models.CharField(max_length=400)
 
 
+    def __str__(self):
+        return self.name
 
-class Jobs(models.Model):
-    title = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
-    preview_url = models.CharField(max_length=400)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
 
 
 class Poster(models.Model):
-        name = models.CharField(max_length=255)
-        photo_url = models.CharField(max_length=400)
-        nationality = models.CharField(max_length=255)
+        image = models.CharField(max_length=255)
+        company = models.CharField(max_length=400)
+
+        def __str__(self):
+            return self.name
+
+
+class Jobs(models.Model):
+    location = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    description = models.CharField(max_length=400)
+    date = models.CharField(max_length=255)
+    compensation = models.CharField(max_length=400)
+    poster = models.ForeignKey(Poster, on_delete=models.CASCADE, related_name='posters')
+
+
+    def __str__(self):
+        return self.name
+
+
+
