f70ad628a1052c6348577b1c0126ccfe8425761a
Will <flmngwllm232@gmail.com>
2018-12-10 21:47:38 -0500

    added to admin page

--- filmconnx/admin.py
+++ filmconnx/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import User, Poster, Jobs
 
 # Register your models here.
+admin.site.register([User, Poster, Jobs])
--- filmconnx/urls.py
+++ filmconnx/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = [
+from django.urls import path, include
+from rest_framework import routers
 
-  ]
+from . import views
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewView)
+router.register('jobs', views.JobsViewView)
+router.register('poster', views.PosterView)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
