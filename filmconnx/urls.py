13a73bf2bd7a8b9ebe468ad1cdf2e9e7bdc9041d
Will <flmngwllm232@gmail.com>
2018-12-11 15:48:59 -0500

    created superuser

--- filmconnx/urls.py
+++ filmconnx/urls.py
@@ -4,8 +4,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('users', views.UserViewView)
-router.register('jobs', views.JobsViewView)
+router.register('users', views.UserView)
+router.register('jobs', views.JobsView)
 router.register('poster', views.PosterView)
 
 
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-if __name__ == '__filmconnx__':
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project4.settings')
     try:
         from django.core.management import execute_from_command_line
