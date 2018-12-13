c050431f20cb527cb5dc9fda9686aa22b4bb209c
dextermichaelh <dextermichael.h@gmail.com>
2018-12-13 14:34:25 -0500

    Login showing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import HomePage from './components/HomePage';
 import Navbar from './components/Navbar';
+import LoginPage from './components/LoginPage';
 
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
 
              {/* <Route exact path="/login" component={Login}></Route> */}
              <Route exact path="/" component={HomePage}></Route>
+             <Route exact path ="/loginpage" component={LoginPage}></Route> 
 
           </Switch>
       </div>
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "reactstrap";
 
 class LoginPage extends Component {
     render() {
         return (
             <div>
-                
+                LOGINPAGE
             </div>
         );
     }
