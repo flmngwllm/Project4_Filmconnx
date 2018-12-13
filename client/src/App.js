b5291466f0b9b38bdd4ae442d6604ffbef74d101
Phillip Cumberlander <cumberlander11@gmail.com>
2018-12-13 10:19:39 -0500

    changed homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import JobPost from './components/JobPost';
+
 
 class App extends Component {
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Router> 
+      <div>
+       <Navbar />
+          <Switch>
+
+             {/* <Route exact path="/login" component={Login}></Route> */}
+             <Route exact path="/postings" component={JobPost}></Route>
+
+          </Switch>
       </div>
+    </Router>
     );
   }
 }
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                
+                hello world
             </div>
         );
     }
