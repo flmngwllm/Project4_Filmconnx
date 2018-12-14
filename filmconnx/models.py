30122dafdd20131e677ac778458a880c17cedd22
dextermichaelh <dextermichael.h@gmail.com>
2018-12-14 15:21:08 -0500

    added to models

--- filmconnx/models.py
+++ filmconnx/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class User(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     name = models.CharField(max_length=50)
     photo = models.CharField(max_length=400)
     location = models.CharField(max_length=255)
@@ -29,6 +31,8 @@ class Position(models.Model):
 
 
 class Poster(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     image = models.CharField(max_length=255)
     company = models.CharField(max_length=400)
