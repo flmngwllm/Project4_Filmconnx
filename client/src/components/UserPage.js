d38743dc77c0e8f3a8674a63c4a2e56ce848dd39
dextermichaelh <dextermichael.h@gmail.com>
2018-12-17 16:53:08 -0500

    hope

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -23,15 +23,45 @@ class UserPage extends Component {
             console.log(this.state.jobs)
         })
     }
-//make a call to display jobs 
-// api/poster/jobs
-    getJobs = () => { 
-        const id = this.params.match.params.userId
-        axios.get(`/api/poster/${id}/jobs`).then(res => {
-            console.log(res.data)
-            this.setState({poster:res.data,jobs:res.data})
+
+    handleCreateNewJob = () => {
+        const id = this.props.match.params.userId
+        const payload = {
+          location: 'location',
+          company: 'company',
+          description: 'description',
+          time: 'time',
+          user: []
+        }
+        axios.post(`/api/user/${id}/jobs`, payload).then(res => {
+          const newJobs = res.data
+          const newStateJobs = [...this.state.jobs, newJobs]
+          this.setState({ trips: newStateJobs })
         })
-    }
+      }
+
+      handleDelete = (jobId) => {
+        console.log(jobId)
+        const id = this.props.match.params.userId
+        axios.delete(`/api/user/${id}/jobs/${jobId}`).then(() => {
+        const newJobs = [...this.state.jobs]
+        const filterJob = newJobs.filter(job =>{
+          return job.id !== jobId
+        })
+    
+        this.setState({jobs: filterJob})
+        })
+      }
+
+      handleUpdate = (jobId) => {
+        const id = this.props.match.params.userId
+        const findJob = this.state.jobs.find(job =>{
+          return job.id === jobId
+        })
+        axios.patch(`/api/user/${id}/trip/${jobId}`, findJob).then(() => {
+          console.log("Updated Job")
+        })
+      }
 
     
 
