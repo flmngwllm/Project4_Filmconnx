666a1ba69c2001476961617a446a7ff943e34fd9
dextermichaelh <dextermichael.h@gmail.com>
2018-12-19 13:40:51 -0500

    hire in nave bar

--- client/src/components/ApplicantPage.js
+++ client/src/components/ApplicantPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Button } from 'reactstrap';
+import { Card,  CardBody,
+    CardTitle,} from 'reactstrap';
   
 import './ApplicantPage.css';
 import axios from 'axios'
